a939d0834026981c8da81a0dff7b53ad6a9f8008
Vlad Kharkovskiy <92023832+VlADOOSit@users.noreply.github.com>
2021-10-20 19:03:58 +0300

    Update libmx.h

--- inc/libmx.h
+++ inc/libmx.h
@@ -5,12 +5,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <malloc/malloc.h>
 #include <fcntl.h>
+
+typedef struct s_list {
+    void*data;
+    struct s_list *next;
+}t_list;
 
 void mx_printchar(char c);
 int mx_strlen(const char* s);
 void mx_printstr(const char* s);
-
+void mx_print_unicode(wchar_t c);
 void mx_print_strarr(char **arr, const char *delim);
 void mx_printint(int n);
 double mx_pow(double n, unsigned int pow);
@@ -54,6 +60,18 @@ void *mx_memccpy(void *restrict dst, const void *restrict src, int c, size_t n);
 int mx_memcmp(const void *s1, const void *s2, size_t n);
 void *mx_memchr(const void *s, int c, size_t n);
 void *mx_memrchr(const void *s, int c, size_t n);
+void *mx_memmem(const void *big, size_t big_len, const void *little, size_t little_len);
+void *mx_memmove(void *dst, const void *src, size_t len);
+void *mx_realloc(void *ptr, size_t size);
+t_list *mx_create_node(void *data);
+void mx_push_front(t_list **list, void *data);
+void mx_push_back(t_list **list, void *data);
+void mx_pop_front(t_list **list);
+void mx_pop_back(t_list **list);
+int mx_list_size(t_list *list);
+t_list *mx_sort_list(t_list *list, bool (*cmp)(void *a, void *b));
+
+
 
 
 #endif
