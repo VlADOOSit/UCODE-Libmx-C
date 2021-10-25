53cfee427d8c76ed861e2437ba1400333490d954
Vlad Kharkovskiy <92023832+VlADOOSit@users.noreply.github.com>
2021-10-25 11:58:42 +0300

    Update mx_read_line.c

--- src/mx_read_line.c
+++ src/mx_read_line.c
@@ -1,30 +1,41 @@
 #include "libmx.h"
 
 int mx_read_line(char **lineptr, size_t buf_size, char delim, const int fd) {
-    if (buf_size < 0 || fd < 0) {
+    if (buf_size < 0 || fd < 0)
         return -2;
-    }
+
     (*lineptr) = (char *) mx_realloc(*lineptr, buf_size);
+    mx_memset((*lineptr), '\0', malloc_size((*lineptr)));
+    size_t count_bytes = 0;
     char buf;
-    size_t result = 0;
-    bool check = false;
-    while(1) {
-        if(!read(fd, &buf, 1) && !check) {
-            return -1;
-        }
-        if (buf == delim) {
-            break;
-        }
-        if (result >= buf_size) {
-            (*lineptr) = (char *) mx_realloc(*lineptr, result + 1);
-        }
-        (*lineptr)[result] = buf;
-        result++;
-        if(!check) {
-            check = true;
-        }
+
+    if (read(fd, &buf, 1)) {
+        if (buf == delim)
+            return count_bytes;
+
+        (*lineptr) = (char *) mx_realloc(*lineptr, count_bytes + 1);
+        (*lineptr)[count_bytes] = buf;
+        count_bytes++;
     }
-    size_t exbytes = malloc_size((*lineptr)) - result;
-    mx_memset(&(*lineptr)[result], '\0', exbytes);
-    return result;
+    else
+        return -1;
+
+    while (read(fd, &buf, 1)) {
+        if (buf == delim)
+            break;
+        
+        if (count_bytes >= buf_size)
+            (*lineptr) = (char *) mx_realloc(*lineptr, count_bytes + 1);
+
+        (*lineptr)[count_bytes] = buf;
+
+        count_bytes++;
+    }
+
+    (*lineptr) = (char *) mx_realloc(*lineptr, count_bytes + 1);
+
+    size_t free_count_bytes = malloc_size((*lineptr)) - count_bytes;
+    mx_memset(&(*lineptr)[count_bytes], '\0', free_count_bytes);
+
+    return count_bytes + 1;
 }
